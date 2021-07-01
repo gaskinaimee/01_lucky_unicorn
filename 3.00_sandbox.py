a65d42252a146ac6578fc3b0a9bd49fc252cd3d3
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-07-02 10:58:11 +1200

    05_basics

--- 3.00_sandbox.py
+++ 3.00_sandbox.py
@@ -1,8 +1,10 @@
 # Ask user for a number.
-get_number = input("Choose a number? ")
+get_number = int(input("Choose a number? "))
 
 # Multiply the number by 5.
 times_five = get_number * 5
 
+answer = ("{} times five is equal to {}.".format(get_number, times_five))
+
 # Output the result.
-print(times_five)
+print(answer)
