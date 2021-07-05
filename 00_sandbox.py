2c4d5a92fdb6400ac53adc44525f616d478c707b
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-07-05 12:24:54 +1200

    answer variable added.

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -4,5 +4,7 @@ get_number = int(input("Choose a number? "))
 # Multiply the number by 5.
 times_five = get_number * 5
 
+answer = ("{} times five is equal to " \
+          "{}.".format(get_number, times_five))
 # Output the result.
-print(times_five)
+print(answer)
