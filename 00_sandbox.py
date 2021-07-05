cd931c58a265bc677d82ad1eb4d276552a3d818c
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-07-05 12:29:05 +1200

    nEW SANDBOX ADDED.

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -8,3 +8,13 @@ answer = ("{} times five is equal to " \
           "{}.".format(get_number, times_five))
 # Output the result.
 print(answer)
+
+# Get name until an exit code is entered...
+
+name = ""
+while name.lower() != "xxx":
+        name = input("Who are you? ")
+        print(name)
+
+print()
+print("We are done!")
