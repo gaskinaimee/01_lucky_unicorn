11431f944e0fb78e722c1a2d208b9872777d6cd3
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-07-06 12:57:45 +1200

    06_looping added.

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,20 +1,9 @@
-# Asks users for a number.
-get_number = int(input("Choose a number? "))
-
-# Multiply the number by 5.
-times_five = get_number * 5
-
-answer = ("{} times five is equal to " \
-          "{}.".format(get_number, times_five))
-# Output the result.
-print(answer)
-
 # Get name until an exit code is entered...
 
 name = ""
 while name.lower() != "xxx":
-        name = input("Who are you? ")
-        print(name)
+    name = input("Who are you? ")
+    print(name)
 
 print()
 print("We are done!")
