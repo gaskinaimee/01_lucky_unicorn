220bf453be0574a9dc8a31148bc4045b911967f9
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-08-12 09:47:48 +1200

    Finished task and added to base_v1.

--- Statement_Decorator_v1.py
+++ Statement_Decorator_v1.py
@@ -1,1 +1,19 @@
-print("$" * 3)
+
+def statement_generator(statement, decoration):
+
+    sides = decoration * 3
+
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
+
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
+
+# Main routine...
+statement_generator("Welcome to the Lucky Unicorn Game", "*")
+print()
+statement_generator("Congratulations, you won a unicorn", "!")
+print()
